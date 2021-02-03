e77d11c2f16abcfaba2a473f4ae0bbd1ddaf85a8
Stefan Jokic <stefanpelejokic@gmail.com>
2021-02-03 19:32:07 +0100

    proxy

--- frontend/src/axios/index.js
+++ frontend/src/axios/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 var axs = axios.create({
-  baseURL: 'http://localhost:8080',
 })
 
 export default axs;
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,14 @@
+const path = require("path");
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  devServer:{
+    proxy:{
+      "/":{
+        target:"http://localhost:8080/"
+      }
+    }
+  }
 }
